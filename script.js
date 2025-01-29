8f798f9cf5aefdaa6de5428d5a134ca5306dcf37
Farman71 <farmanhussain71@gmail.com>
2025-01-29 17:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let fName=document.getElementById("fname");
+	let lName=document.getElementById("lname");
+	alert(fName.value+" "+lName.value)
 }
